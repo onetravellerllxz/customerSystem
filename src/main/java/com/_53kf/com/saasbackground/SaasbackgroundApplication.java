29a6a0bb997a870db44326107f3bd19a186a6e30
onetravellerllxz <onetraveller@sina.cn>
2020-01-07 11:06:13 +0800

    新建项目

---
+++ src/main/java/com/_53kf/com/saasbackground/SaasbackgroundApplication.java
@@ -0,0 +1,13 @@
+package com._53kf.com.saasbackground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasbackgroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaasbackgroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/_53kf/com/saasbackground/SaasbackgroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com._53kf.com.saasbackground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasbackgroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
